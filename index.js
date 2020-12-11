2c5d17dd9c428e92e38a8064ecad7887d6d9b19a
masam101376 <ma.sa.m10.1.3.7.6@gmail.com>
2020-12-11 23:22:37 +0900

    Refactored.

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const judge(arg) => {
 
 // ボタンをクリックしたら正誤判定
 document.getElementsByTagName('button')[0].addEventListener('click', () => {
-    if(document.getElementsByTagName('button')[0].textContent === correct){
+    if(button[0].textContent === correct){
         alert('正解です');        
     } else {
         alert('間違っています');
