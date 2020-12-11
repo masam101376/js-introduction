b38fecb579a4510f75d9cc02e72bb3e6b5dc4be9
masam101376 <ma.sa.m10.1.3.7.6@gmail.com>
2020-12-11 23:22:09 +0900

    Refactored codes around buttons.

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const correct = 'ニンテンドーDS';
 
 document.getElementById('js-question').textContent = question;
 
-document.getElementsByTagName('button')[0].textContent = answers[0];
-document.getElementsByTagName('button')[1].textContent = answers[1];
-document.getElementsByTagName('button')[2].textContent = answers[2];
-document.getElementsByTagName('button')[3].textContent = answers[3];
+const button = document.getElementsByTagName('button');
+
+button[0].textContent = answers[0];
+button[1].textContent = answers[1];
+button[2].textContent = answers[2];
+button[3].textContent = answers[3];
 
 //正誤判定をするための関数
 /*
