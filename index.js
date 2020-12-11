47cd28fe1ee7f7c4a43714418b9506891830695d
masam101376 <ma.sa.m10.1.3.7.6@gmail.com>
2020-12-11 23:24:24 +0900

    Refactored.

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ const correct = 'ニンテンドーDS';
 
 document.getElementById('js-question').textContent = question;
 
-const button = document.getElementsByTagName('button');
+// 難度も出てくるものは変数, 定数に入れてしまう
+// HTMLのオブジェクトをとってくる場合、変数/定数名の頭に$をつける
+const $button = document.getElementsByTagName('button');
 
-button[0].textContent = answers[0];
-button[1].textContent = answers[1];
-button[2].textContent = answers[2];
-button[3].textContent = answers[3];
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
 
 //正誤判定をするための関数
 /*
@@ -28,8 +30,8 @@ const judge(arg) => {
 */
 
 // ボタンをクリックしたら正誤判定
-document.getElementsByTagName('button')[0].addEventListener('click', () => {
-    if(button[0].textContent === correct){
+$button[0].addEventListener('click', () => {
+    if($button[0].textContent === correct){
         alert('正解です');        
     } else {
         alert('間違っています');
