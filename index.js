3c85b0b1249ef8b67f101ee17778220fe6a5e609
masam101376 <ma.sa.m10.1.3.7.6@gmail.com>
2020-12-12 01:54:18 +0900

    Calculate score.

--- index.js
+++ index.js
@@ -30,60 +30,58 @@ const quiz = [
     }
 ]
 
-console.log(quiz[0].question);
-
 // 何度も出てくるものは変数, 定数に入れてしまう
 // HTMLのオブジェクトをとってくる場合、変数/定数名の頭に$をつける
+const quizLength = quiz.length;
+let quizIndex = 0;
+
 const $question = document.getElementById('js-question');
 const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length;
 let score = 0;
 
-
-
-// 簡易フローチャート
-// 一問目のクイズをセットアップする
+// クイズをセットアップする
 const setUpQuiz = () => {
     // 質問文を定義
-    $question.textContent = quiz[0].question;
+    $question.textContent = quiz[quizIndex].question;
 
     let buttonIndex = 0;
     while(buttonIndex < buttonLength){
-        $button[buttonIndex].textContent = quiz[0].answers[buttonIndex];
+        $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
         buttonIndex++;
     }
 }
 setUpQuiz();
 
-
 // 正誤判定する関数を定義
-const judgeAnswer = (e) => {
-    if(quiz[0].correct === e.target.textContent){
-        score += 1;        
+const clickHandler = (e) => {
+    if (quiz[quizIndex].correct === e.target.textContent) {
+        // 正解ならばこちらを実行
+        score += 1;
     } else {
-        score += 0
+        // 不正解ならばこちらを実行
+        score += 0;
+    }
+
+    quizIndex++;
+
+    if(quizIndex < quizLength){
+        // 問題数がまだあればこちらを実行
+        setUpQuiz();
+    } else {
+        // 問題数がもうなければこちらを実行
+        window.alert('終了！');
+        window.alert(score);
     }
     console.log(score);
 }
 
-/*
-// ボタンをクリックしたら正誤判定
-let judgeAnswerIndex = 0;
-while(judgeAnswerIndex < buttonLength){
-    $button[judgeAnswerIndex].addEventListener('click', (e) => {
-        judgeAnswer(e);
-    });
-    judgeAnswerIndex++;
-}
-*/
 
-  // 一問目のクイズに答える
-        // 不正解 → アラートで不正解とする
-        // 正解 → 次へ
-    // 二問目のクイズに答える
-        // 不正解 → アラートで不正解とする
-        // 正解 → 次へ
-    // 3問目のクイズに答える
-        // 不正解 → アラートで不正解とする
-        // 正解 → 点数と戻るボタンを表示する
-    // 戻るボタンを押すと一問目のクイズの画面に戻る
+// ボタンをクリックしたら正誤判定
+let clickHandlerIndex = 0;
+while(clickHandlerIndex < buttonLength){
+    $button[clickHandlerIndex].addEventListener('click', (e) => {
+        clickHandler(e);
+    });
+    clickHandlerIndex++;
+}
